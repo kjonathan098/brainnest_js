96bbb8445d9492a1496232b168378bb498982a76
Jonathan Kelman <kelman.jonathan@gmail.com>
2023-01-29 21:56:11 +0200

    task 3

---
+++ task3.js
@@ -0,0 +1,15 @@
+// Task 3-1
+
+function getLargerNum(num1, num2) {
+	return Math.max(num1, num2)
+}
+
+console.log(getLargerNum(1, 15))
+
+// Task 3-2
+
+function typeOfNum(num) {
+	return num % 2 === 0 ? `${num} is even` : `${num} is odd`
+}
+
+console.log(typeOfNum(653))
